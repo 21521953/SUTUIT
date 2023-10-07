6b85888fcc6c4feb7f6ac5dfd0a3b97e13509b32
Luong Thi Thuy Diem <21521953@gm.uit.edu.vn>
2023-10-07 19:30:42 +0700

    Set up morgan, node-sass

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@
 const path = require('path');
 const express = require('express');
 const handlebars = require('express-handlebars');
+const morgan = require('morgan');
 const app = express();
 const port = 3000;
 
-
+app.use(express.static( path.join(__dirname,'public')))
+// http logger
+app.use(morgan('combined'));
 // template handlebar
 const hbs = handlebars.create({ defaultLayout: 'main' });
 app.engine('handlebars', hbs.engine);
